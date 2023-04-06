c5dd6ca685d09b65d6a97cc4fe33c23a6670eb47
Jose Cepero <ceperojose54@gmail.com>
2023-04-06 05:19:19 -0400

    finished contact section

--- src/index.js
+++ src/index.js
@@ -68,6 +68,13 @@ footLink.classList.add('bold');
 // Contact 
 const contact = document.querySelector('section.contact');
 contact.querySelector('h4').textContent = siteContent.contact['contact-h4'];
+ contact.children[1].textContent = siteContent.contact.address;
+ contact.children[2].textContent = siteContent.contact.phone;
+ contact.children[3].textContent = siteContent.contact.email;
+
+
+ //main content
+
 
 
 
